1f6ddf5725d698bee36f57b094a7d81e259d071c
user <user@DESKTOP-7EQF3M8>
2023-01-10 15:05:17 +0900

    1

--- pro1/src/pro1/Aclass.java
+++ pro1/src/pro1/Aclass.java
@@ -4,6 +4,6 @@ public class Aclass {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		System.out.println("MOYANOYA1212");
+		System.out.println("MOYANOYA");
 	}
 }
